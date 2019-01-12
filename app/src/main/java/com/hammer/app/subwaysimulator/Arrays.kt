8cc5ca82fa5e55b653340c68ce9a4eaf59faade5
Takanori Sato <hammerxhammer0802@gmail.com>
2019-01-12 23:06:06 +0900

    新メニューと期間終了メニュー対応

--- app/src/main/java/com/hammer/app/subwaysimulator/Arrays.kt
+++ app/src/main/java/com/hammer/app/subwaysimulator/Arrays.kt
@@ -19,10 +19,13 @@ enum class Sandwiches(val number: Int, val sandName: String, val price: Int, val
     AVOCADO_VEGE(12,"アボカドベジー", 410, Dressings.CREAMY.number),
     VEGE_CHEESE(13, "ベジー＆チーズ",340, Dressings.CREAMY.number),
     VEGE_DE_LIGHT(14,"ベジーデライト",300, Dressings.OIL_VINEGAR.number),
-    KINKAKU_BURG(15,"金格バーグ(期間限定)",890, Dressings.WAFU.number),
-    KINKAKU_DX(16,"金格DX(期間限定)",990, Dressings.WAFU.number),
-    BACON_CHICKEN_MELT(17, "ベーコンチキンメルト(期間限定)", 510, Dressings.RANCH.number),
-    CHICKEN_MELT(18, "チキンメルト(期間限定)", 450, Dressings.RANCH.number),
+    MOZZARELLA_CHIKEN(15, "大人モッツァレラ・チキン(期間限定)", 490, Dressings.BASIL.number),
+
+    //期間終了
+    KINKAKU_BURG(16,"金格バーグ(期間限定)",890, Dressings.WAFU.number, false),
+    KINKAKU_DX(17,"金格DX(期間限定)",990, Dressings.WAFU.number, false),
+    BACON_CHICKEN_MELT(18, "ベーコンチキンメルト(期間限定)", 510, Dressings.RANCH.number, false),
+    CHICKEN_MELT(19, "チキンメルト(期間限定)", 450, Dressings.RANCH.number, false),
 }
 
 enum class Breads(val number: Int, val breadName: String, val price: Int = 0, val isEnabled:Boolean = true) {
@@ -32,7 +35,9 @@ enum class Breads(val number: Int, val breadName: String, val price: Int = 0, va
     HONEY_OATES(3, "ハニーオーツ"),
     FLAT_BREAD(4, "フラットブレッド"),
     NONE(5,"無し(サラダ, + 300円)", 300),
-    ONION_SESAME(6,"オニオンセサミペッパー(期間限定)",0),
+
+    //期間終了
+    ONION_SESAME(6,"オニオンセサミペッパー(期間限定)", 0, false),
 
 }
 
@@ -46,7 +51,9 @@ enum class Toppings(val toppingName: String, val price: Int, val engName: String
     SHRIMP("えび(+ 100円)",100, "shrimp"),
     AVOCADO("アボカド(+ 110円)",110,"avocado"),
     ROAST_BEEF("ローストビーフ(+ 350円)", 350, "roastbeef"),
-    SHREDDED_CHEESE("シュレッドチーズ(+ 40円)", 40, "shredded"),
+
+    //期間終了
+    SHREDDED_CHEESE("シュレッドチーズ(+ 40円)", 40, "shredded", false),
 }
 
 //NONEを最後にしないとエラるのでFalseのclassの直前にする
@@ -62,9 +69,11 @@ enum class Dressings(val number: Int, val dressingName: String, val isEnabled:Bo
     CHILI(8,"チリソース（激辛)"),
     SALT_PEPPER(9, "塩・こしょう"),
     PEPPER(10, "こしょう"),
-    WAFU(11, "特製和風ソース(期間限定)"),
-    RANCH(12, "ランチドレッシング(期間限定)"),
-    NONE(13, "無し"),
+    NONE(11, "無し"),
+
+    //期間終了
+    WAFU(12, "特製和風ソース(期間限定)", false),
+    RANCH(13, "ランチドレッシング(期間限定)", false),
 
 }
 
