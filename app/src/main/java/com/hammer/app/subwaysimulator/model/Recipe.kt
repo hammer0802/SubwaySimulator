2def7dbdcc17ab9c41308aa36a5598b184d1fff0
Takanori Sato <taka_sato@yumemi.co.jp>
2021-11-23 16:13:05 +0900

    Recipeをdata classにした

--- app/src/main/java/com/hammer/app/subwaysimulator/model/Recipe.kt
+++ app/src/main/java/com/hammer/app/subwaysimulator/model/Recipe.kt
@@ -1,49 +1,49 @@
 package com.hammer.app.subwaysimulator.model
 
-class Recipe {
-    var name = ""  //レシピ名
-    var price = 0  //合計金額
-    var sandwich = "" //サンドウィッチの種類
-    var footLong = false
-    var bread = ""  //パンの種類
-    var toast = false //トーストの有無
+data class Recipe (
+    var name: String = "",  //レシピ名
+    var price: Int = 0,  //合計金額
+    var sandwich: String = "", //サンドウィッチの種類
+    var footLong: Boolean = false,
+    var bread: String = "",  //パンの種類
+    var toast: Boolean = false, //トーストの有無
     //トッピング、煩雑なのでToppingクラスでまとめる？
-    var cheese = false
-    var cream = false
-    var mascar = false
-    var egg = false
-    var bacon = false
-    var tuna = false
-    var shrimp = false
-    var avocado = false
-    var roastbeef = false
-    var cheeseAmount = 1
-    var creamAmount = 1
-    var mascarAmount = 1
-    var eggAmount = 1
-    var baconAmount = 1
-    var tunaAmount = 1
-    var shrimpAmount = 1
-    var avocadoAmount = 1
-    var roastbeefAmount = 1
+    var cheese: Boolean = false,
+    var cream: Boolean = false,
+    var mascar: Boolean = false,
+    var egg: Boolean = false,
+    var bacon: Boolean = false,
+    var tuna: Boolean = false,
+    var shrimp: Boolean = false,
+    var avocado: Boolean = false,
+    var roastbeef: Boolean = false,
+    var cheeseAmount: Int = 1,
+    var creamAmount: Int = 1,
+    var mascarAmount: Int = 1,
+    var eggAmount: Int = 1,
+    var baconAmount: Int = 1,
+    var tunaAmount: Int = 1,
+    var shrimpAmount: Int = 1,
+    var avocadoAmount: Int = 1,
+    var roastbeefAmount: Int = 1,
 
     //期間限定トッピング
-    var shredded = false
-    var shreddedAmount = 1
+    var shredded: Boolean = false,
+    var shreddedAmount: Int = 1,
 
     //野菜
-    var lettuce = ""
-    var tomato = ""
-    var greenpepper = ""
-    var redonion = ""
-    var carrot = ""
-    var olive = ""
-    var pickles = ""
-    var hotpepper = ""
-    var dressing = mutableListOf<String>()  //ドレッシングの種類
-    var dressingAmount = mutableListOf<String>() //ドレッシングの量
-    var howToDress = ""
-    var createTime = "" //作成日時
-    var editTime = "" //編集日時
-    var uuid = "" //UUID
-}
+    var lettuce: String = "",
+    var tomato: String = "",
+    var greenpepper: String = "",
+    var redonion: String = "",
+    var carrot: String = "",
+    var olive: String = "",
+    var pickles: String = "",
+    var hotpepper: String = "",
+    var dressing: MutableList<String> = mutableListOf<String>(),  //ドレッシングの種類
+    var dressingAmount: MutableList<String> = mutableListOf<String>(), //ドレッシングの量
+    var howToDress: String = "",
+    var createTime: String = "", //作成日時
+    var editTime: String = "", //編集日時
+    var uuid: String = "", //UUID
+)
