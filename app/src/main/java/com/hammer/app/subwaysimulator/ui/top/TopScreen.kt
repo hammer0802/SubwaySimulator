848651016c5061b16cc8ef6bb82eacc4861f0968
Takanori Sato <taka_sato@yumemi.co.jp>
2021-12-04 20:42:46 +0900

    TopAppBarを表示

--- app/src/main/java/com/hammer/app/subwaysimulator/ui/top/TopScreen.kt
+++ app/src/main/java/com/hammer/app/subwaysimulator/ui/top/TopScreen.kt
@@ -1,24 +1,66 @@
 package com.hammer.app.subwaysimulator.ui.top
 
-import android.graphics.Paint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hammer.app.subwaysimulator.R
 
 @Composable
 fun TopScreen() {
+    var showMenu by remember { mutableStateOf(false) }
+
     MaterialTheme {
-        Box(modifier = Modifier.fillMaxSize().background(color = Color.White)) {
-            Text(text = "Hello, Compose!", modifier = Modifier.align(Alignment.Center))
-        }
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White),
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(id = R.string.app_name))
+                    },
+                    actions = {
+                        IconButton(onClick = { showMenu = true }) {
+                            Icon(Icons.Filled.Menu, "menu")
+                        }
+                        DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
+                            DropdownMenuItem(onClick = { showMenu = false }) {
+                                Text(text = stringResource(id = R.string.action_settings))
+                            }
+                            DropdownMenuItem(onClick = { showMenu = false }) {
+                                Text(text = stringResource(id = R.string.action_policy))
+                            }
+                        }
+                    },
+                    backgroundColor = colorResource(id = R.color.colorPrimary),
+                    contentColor = Color.White,
+                    elevation = 12.dp
+                )
+                     },
+            content = {}
+        )
     }
 }
 
