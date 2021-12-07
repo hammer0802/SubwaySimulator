3cba5421800a778de094dfe1c2152310d84870d6
Takanori Sato <taka_sato@yumemi.co.jp>
2021-12-07 22:36:07 +0900

    Spinner作成

--- app/src/main/java/com/hammer/app/subwaysimulator/ui/edit/EditScreen.kt
+++ app/src/main/java/com/hammer/app/subwaysimulator/ui/edit/EditScreen.kt
@@ -1,18 +1,28 @@
 package com.hammer.app.subwaysimulator.ui.edit
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -22,24 +32,38 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hammer.app.subwaysimulator.R
+import com.hammer.app.subwaysimulator.localdata.breads
+import com.hammer.app.subwaysimulator.localdata.sandwiches
+import com.hammer.app.subwaysimulator.localdata.toppings
 
 @Composable
 fun EditScreen() {
     var recipeName by remember { mutableStateOf("") }
-    Column(modifier = Modifier.background(color = Color.White)) {
-        Title(text = stringResource(id = R.string.name_edit))
-        OutlinedTextField(
-            value = recipeName,
-            onValueChange = { recipeName = it },
-            modifier = Modifier.fillMaxWidth(),
-            colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = colorResource(id = R.color.colorAccent))
-        )
-        Title(text = stringResource(id = R.string.step1))
-        Title(text = stringResource(id = R.string.step2))
-        Title(text = stringResource(id = R.string.step3))
-        Title(text = stringResource(id = R.string.step4))
-        Title(text = stringResource(id = R.string.step5))
-        Title(text = stringResource(id = R.string.step5_2))
+    MaterialTheme {
+        Column(modifier = Modifier.background(color = Color.White)) {
+            Title(text = stringResource(id = R.string.name_edit))
+            OutlinedTextField(
+                value = recipeName,
+                onValueChange = { recipeName = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = colorResource(id = R.color.colorAccent))
+            )
+            // TODO: TypedArrayで使ってるところを消したらtoListを消す
+            // sandwiches
+            Title(text = stringResource(id = R.string.step1))
+            Spinner(list = sandwiches.toList())
+            // breads
+            Title(text = stringResource(id = R.string.step2))
+            Spinner(list = breads.toList())
+            // toppings
+            Title(text = stringResource(id = R.string.step3))
+            Spinner(list = toppings.toList())
+            Title(text = stringResource(id = R.string.step4))
+            Title(text = stringResource(id = R.string.step5))
+            Title(text = stringResource(id = R.string.step5_2))
+        }
     }
 }
 
@@ -52,6 +76,33 @@ private fun Title(text: String) {
             .background(color = colorResource(id = R.color.colorPrimary))
     ) {
         Text(modifier = Modifier.padding(4.dp), text = text, color = Color.White, style = TextStyle(fontWeight = FontWeight.Bold))
+    }
+}
+
+@Composable
+private fun Spinner(list: List<String>) {
+    var selectedItemName by remember { mutableStateOf(list.first()) }
+    var showMenu by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { showMenu = true },
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(modifier = Modifier.padding(4.dp), text = selectedItemName)
+        Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "ArrowDropDown")
+    }
+    DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
+        list.forEach {
+            DropdownMenuItem(onClick = {
+                selectedItemName = it
+                showMenu = false
+            }) {
+                Text(text = it)
+            }
+        }
     }
 }
 
