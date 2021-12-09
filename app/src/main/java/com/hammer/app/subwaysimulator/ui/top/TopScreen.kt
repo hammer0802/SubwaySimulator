b9c142c99b8be98a58c9a0267a3ecb676efe554e
Takanori Sato <taka_sato@yumemi.co.jp>
2021-12-09 16:06:23 +0900

    FloatingActionButtonをadView分だけ浮かせた

--- app/src/main/java/com/hammer/app/subwaysimulator/ui/top/TopScreen.kt
+++ app/src/main/java/com/hammer/app/subwaysimulator/ui/top/TopScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -113,6 +114,7 @@ fun TopScreen(recipeList: List<Recipe>, topViewModel: TopViewModel = viewModel()
                     onClick = {
                         topViewModel.openCreateRecipeScreen()
                     },
+                    modifier = Modifier.offset(y = (-50).dp),
                     backgroundColor = colorResource(id = R.color.colorAccent),
                     contentColor = Color.White,
                 ) {
