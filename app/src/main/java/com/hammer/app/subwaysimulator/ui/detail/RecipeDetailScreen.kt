462c517969d9c2347b137540fc84e99c3cbef501
Takanori Sato <taka_sato@yumemi.co.jp>
2021-12-11 17:45:34 +0900

    backgroundに画像表示

--- app/src/main/java/com/hammer/app/subwaysimulator/ui/detail/RecipeDetailScreen.kt
+++ app/src/main/java/com/hammer/app/subwaysimulator/ui/detail/RecipeDetailScreen.kt
@@ -1,7 +1,15 @@
 package com.hammer.app.subwaysimulator.ui.detail
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
@@ -19,7 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -66,7 +76,28 @@ fun RecipeDetailScreen() {
                     elevation = 12.dp
                 )
             },
-            content = {},
+            content = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 8.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.oldpaper),
+                        contentDescription = "background",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .verticalScroll(rememberScrollState())
+                    ) {}
+                }
+            },
         )
     }
 }
