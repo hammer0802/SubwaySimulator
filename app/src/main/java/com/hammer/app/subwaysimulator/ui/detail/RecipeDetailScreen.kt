0cb779b44bc1a2d2b5e5369456d469c8e4fd4ac0
Takanori Sato <taka_sato@yumemi.co.jp>
2021-12-11 17:14:30 +0900

    topBar実装

--- app/src/main/java/com/hammer/app/subwaysimulator/ui/detail/RecipeDetailScreen.kt
+++ app/src/main/java/com/hammer/app/subwaysimulator/ui/detail/RecipeDetailScreen.kt
@@ -1,10 +1,74 @@
 package com.hammer.app.subwaysimulator.ui.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hammer.app.subwaysimulator.R
 
 @Composable
 fun RecipeDetailScreen() {
+    var showMenu by remember { mutableStateOf(false) }
+
+    MaterialTheme {
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White),
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(id = R.string.recipe_detail))
+                    },
+                    actions = {
+                        IconButton(onClick = { showMenu = true }) {
+                            Icon(Icons.Filled.Menu, "menu")
+                        }
+                        DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
+                            DropdownMenuItem(onClick = {
+                                showMenu = false
+                            }) {
+                                Text(text = stringResource(id = R.string.action_edit))
+                            }
+                            DropdownMenuItem(onClick = {
+                                showMenu = false
+                            }) {
+                                Text(text = stringResource(id = R.string.action_sns))
+                            }
+                            DropdownMenuItem(onClick = {
+                                showMenu = false
+                            }) {
+                                Text(text = stringResource(id = R.string.action_save_image))
+                            }
+                        }
+                    },
+                    backgroundColor = colorResource(id = R.color.colorPrimary),
+                    contentColor = Color.White,
+                    elevation = 12.dp
+                )
+            },
+            content = {},
+        )
+    }
 }
 
 @Preview
