df03b1f45a983da0c1a60260e4bb845b06307e5a
Takanori Sato <taka_sato@yumemi.co.jp>
2021-12-11 17:09:41 +0900

    RecipeDetailScreenの追加

---
+++ app/src/main/java/com/hammer/app/subwaysimulator/ui/detail/RecipeDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.hammer.app.subwaysimulator.ui.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RecipeDetailScreen() {
+}
+
+@Preview
+@Composable
+fun PreviewRecipeDetailScreen() {
+    RecipeDetailScreen()
+}
